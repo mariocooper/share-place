53b7f0f1952a86873b3e52ec5067452bc311fb31
Mario Cooper <mariocooper00@hotmail.com>
2023-09-16 18:09:16 +0100

    NavLinks & SideDrawer added - added React portal for SideDrawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,26 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 import "./MainNavigation.css";
 
 const MainNavigation = props => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">
-          SharePlace
-        </Link>
-      </h1>
-      <nav>
-        ...
-      </nav>
-    </MainHeader>
-  )
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">
+            SharePlace
+          </Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
+  );
 };
 
 export default MainNavigation;
