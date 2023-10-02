3ef493a219e2134e3499f93333b5fa7ac175ad42
Mario Cooper <mariocooper00@hotmail.com>
2023-10-02 21:27:50 +0100

    loading places by UserId

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,39 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom/cjs/react-router-dom.min";
+import { useHttpClient } from "../../shared/hooks/http-hook";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import PlaceList from "../components/PlaceList";
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-      title: "Empire State Building",
-      description: "The world's most magnificent Art Deco skyscraper",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/df/NYC_Empire_State_Building.jpg/640px-NYC_Empire_State_Building.jpg",
-      address: "20 W 34th St., New York, NY 10001, USA",
-      location: {
-        lat: 40.7484405,
-        lng: -73.9878584
-      },
-      creator: "u1"
-    },
-    {
-      id: "p2",
-      title: "Statue of Liberty",
-      description: "The statue is a personification of liberty in the form of a woman",
-      imageUrl: "https://upload.wikimedia.org/wikipedia/commons/a/af/Statue_of_liberty_side.jpg",
-      address: "New York, NY 10004, USA",
-      location: {
-        lat: 40.6892494,
-        lng: -74.0445004
-      },
-      creator: "u2",
-    }
-  ];
-
 const UserPlaces = () => {
+  const [loadedPlaces, setLoadedPlaces] = useState();
+  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+  
   const userId = useParams().userId;
-  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+
+  useEffect(() => {
+    const fetchPlaces = async () => {
+      try {
+        const responseData = await sendRequest(
+          `http://localhost:5000/api/places/user/${userId}`
+        );
+        setLoadedPlaces(responseData.places);
+      } catch (err) {}
+    };
+    fetchPlaces();
+  }, [sendRequest, userId]);
+
   return (
-    <PlaceList items={loadedPlaces} />
+    <>
+      <ErrorModal error={error} onClear={clearError} />
+      {isLoading && (
+        <div className="center">
+          <LoadingSpinner />
+        </div>
+      )}
+      {!isLoading && loadedPlaces && <PlaceList items={loadedPlaces} />}
+    </>
   );
 };
 
