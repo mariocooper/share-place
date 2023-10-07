c2937150bdc678a6e7fd00ec40b44fe714965cd0
Mario Cooper <mariocooper00@hotmail.com>
2023-10-07 11:16:27 +0100

    now storing token in browser storage

--- src/App.js
+++ src/App.js
@@ -14,6 +14,10 @@ const App = () => {
 
   const login = useCallback((uid, token) => {
     setToken(token);
+    localStorage.setItem(
+      'userData',
+      JSON.stringify({ userId: uid, token: token })
+    );
     setUserId(uid);
   }, []);
 
